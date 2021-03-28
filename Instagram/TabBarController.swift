81a86f97c8d0b55e40a51b1071b9f8d0e386f3ab
toto727 <80440486+toto727@users.noreply.github.com>
2021-03-28 22:04:21 +0900

    Chapter 6まで完成

--- Instagram/TabBarController.swift
+++ Instagram/TabBarController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
 
@@ -17,6 +18,15 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
         // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
         self.delegate = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+         super.viewDidAppear(animated)
+        // currentUserがnilならログインしていない
+                if Auth.auth().currentUser == nil {
+                    // ログインしていないときの処理
+                    let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+                    self.present(loginViewController!, animated: true, completion: nil)
+                }
     }
 
     // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する。
