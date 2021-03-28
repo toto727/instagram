c2937a761a799ad919b9c1672f4a554359b1d961
toto727 <80440486+toto727@users.noreply.github.com>
2021-03-28 22:50:48 +0900

    Chapter 8まで完成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 永嶋瞬 on 2021/03/28.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
